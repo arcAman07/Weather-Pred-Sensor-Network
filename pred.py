ce9044338b115df9b7dd78da0cdd65bc1ad2f9c1
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2022-10-18 17:12:30 +0530

    Update pred.py

--- pred.py
+++ pred.py
@@ -19,4 +19,48 @@ from sklearn import preprocessing
 label_encoder = preprocessing.LabelEncoder()
 filename = 'finalized_model.sav'
 # load the model from disk
-loaded_model = pickle.load(open(filename, 'rb'))
+clf = pickle.load(open(filename, 'rb'))
+from sklearn.preprocessing import StandardScaler
+test_file = pd.read_csv("/Users/deepaksharma/Downloads/CoolTerm Capture 2022-10-18 13-45-56.txt")
+scaler = StandardScaler()
+scaler.fit(test_file)
+test_file = scaler.transform(test_file)
+test_file = test_file.drop(["Temperature (F)","Apparent Temperature (F)"], axis=1)
+test_df = pd.get_dummies(test_file, columns = ['Precip Type'])
+test_df["Precip Type_1"] = 0 # Precip Type_0 correponds to Precip Type_rain, vice versa
+test_df # Precipe Type_1 corresponds to Precip Type_snow which means other conditions/ weather
+y_pred = clf.predict(test_df)
+print(y_pred) # a[19] = 'Partly Cloudy'
+# a[26] = ''Windy and Partly Cloudy'
+"""
+'Breezy', 
+'Breezy and Dry', 
+'Breezy and Foggy',
+'Breezy and Mostly Cloudy', 
+'Breezy and Overcast',
+'Breezy and Partly Cloudy', 
+'Clear',
+'Dangerously Windy and Partly Cloudy', 
+'Drizzle', 
+'Dry',
+'Dry and Mostly Cloudy', 
+'Dry and Partly Cloudy', 
+'Foggy',
+'Humid and Mostly Cloudy', 
+'Humid and Overcast',
+'Humid and Partly Cloudy', 
+'Light Rain', 
+'Mostly Cloudy',
+'Overcast', 
+'Partly Cloudy', 
+'Rain', 
+'Windy', 
+'Windy and Dry',
+'Windy and Foggy', 
+'Windy and Mostly Cloudy', 
+'Windy and Overcast',
+'Windy and Partly Cloudy'
+
+"""
+
+
