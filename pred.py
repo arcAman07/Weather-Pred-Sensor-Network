dc0cd6738292c7ab251b62a5d6bae148ccca96fb
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2022-10-18 17:02:44 +0530

    Update pred.py

--- pred.py
+++ pred.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+import seaborn              as sns
+import plotly.express       as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+import holoviews as hv
+from holoviews import opts
+hv.extension('bokeh')
 
+import warnings
+warnings.filterwarnings("ignore")
+import pickle
